8a4b3d064a0a52e6a77e83c4e43746122476e138
Nikhil <84772872+tymnec@users.noreply.github.com>
2025-02-09 15:54:34 +1100

    fix vehicle code

--- OZFleet.Core/Entities/Vehicle.cs
+++ OZFleet.Core/Entities/Vehicle.cs
@@ -2,25 +2,9 @@ namespace OZFleet.Core.Entities
 {
     public class Vehicle
     {
-        public int Id { get; private set; }
-        public string Model { get; private set; }
-        public string RegistrationNumber { get; private set; }
-        public Driver AssignedDriver { get; private set; }
-
-        // Constructor
-        public Vehicle(int id, string model, string registrationNumber)
-        {
-            Id = id;
-            Model = model;
-            RegistrationNumber = registrationNumber;
-            AssignedDriver = null;  // Initially no driver assigned
-        }
-
-        // Business Logic: Assign Driver to Vehicle
-        public void AssignDriver(Driver driver)
-        {
-            if (driver == null) throw new ArgumentNullException(nameof(driver));
-            AssignedDriver = driver;
-        }
+        public int Id { get; set; }
+        public string Make { get; set; }    // e.g. Toyota
+        public string Model { get; set; }   // e.g. Camry
+        public int Year { get; set; }
     }
 }
